bd1fc2ebeec9743c003f1a6c1dbe8e8df368ddb7
HamielKuo <hamiel.joker@gmail.com>
2015-01-20 23:21:21 +0800

    Rout
    路由

--- 04_ParseRequest/server.js
+++ 04_ParseRequest/server.js
@@ -1,12 +1,13 @@
 var http=require("http");
 var url=require("url");//注入url 這個模組來找出request 的url路徑
 
-function start() {
+function start(route) {
 	// body...
 	function onRequest(request,response) {
 		console.log("Rqeuest received");
 		var pathname=url.parse(request.url).pathname;
 		console.log("Request for " +pathname+" received.");
+		route(pathname);
 		response.writeHead(200,{"Content-Type":"text/plain"});
 		response.write("Hello world");
 		response.end();
