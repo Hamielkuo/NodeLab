c024bb1d1ce8682b10752904de2f99f622dc4850
HamielKuo <hamiel.joker@gmail.com>
2015-01-20 22:32:40 +0800

    傳遞

--- 02_baseHttpServer/server.js
+++ 02_baseHttpServer/server.js
@@ -1,8 +1,7 @@
 var http=require("http");
-http.createServer(
-	function (request,response) {
+function onRequest(request,response) {
 		response.writeHead(200,{"Content-Type":"text/plain"});
 		response.write("Hello world");
 		response.end();
-	}
-	).listen(8888);
+}
+http.createServer(onRequest).listen(8888);
