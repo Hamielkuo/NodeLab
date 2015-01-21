fb7e35b865b28cacfdd54b43b2c1800d041e8d45
HamielKuo <hamiel.joker@gmail.com>
2015-01-21 23:53:17 +0800

    尚未加上return的方式

---
+++ 05_Route/server.js
@@ -0,0 +1,20 @@
+var http=require("http");
+var url=require("url");//注入url 這個模組來找出request 的url路徑
+
+function start(route, handle) {
+	// body...
+	function onRequest(request,response) {
+		console.log("Rqeuest received");
+		var pathname=url.parse(request.url).pathname;
+		console.log("Request for " +pathname+" received.");
+		route(handle, pathname);
+		response.writeHead(200,{"Content-Type":"text/plain"});
+		response.write("Hello world");
+		response.end();
+	}
+	http.createServer(onRequest).listen(8888);
+
+	console.log("Server has started");
+}
+//匯出start這個函數
+exports.start=start;
